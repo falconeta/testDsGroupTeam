6acec6205fc34ed0c430837a1b954a9294db0ae8
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2018-03-08 14:45:36 +0100

    integrate chiamate ajax get post put e delete

--- source/javascript/form.js
+++ source/javascript/form.js
@@ -3,7 +3,8 @@ function load(arrayId, arrayValue) {
   "use strict";
   $("#" + arrayId).attr("value", arrayValue);
 }
-function loadForm(user) {//binding form in modalità modify
+function loadForm(user) {
+  //binding form in modalità modify
   "use strict";
   var arrayId = [
     "name",
@@ -13,16 +14,18 @@ function loadForm(user) {//binding form in modalità modify
     "city",
     "zipcode",
     "phone",
-    "website"];
+    "website"
+  ];
   var arrayValue = [
-    user.name, 
-    user.username, 
-    user.email, 
+    user.name,
+    user.username,
+    user.email,
     user.address.street,
     user.address.city,
     user.address.zipcode,
     user.phone,
-    user.website];
+    user.website
+  ];
   for (var i = 0; i < arrayId.length; i++) {
     load(arrayId[i], arrayValue[i]);
   }
@@ -33,16 +36,71 @@ window.addEventListener("load", function() {
   switch (this.localStorage.getItem("selector")) {
     case "create":
       console.log("create");
+      $("#submit").click(function() {
+        $.ajax({
+          url: "https://jsonplaceholder.typicode.com/users",
+          type: "post",
+          data: $("#contact").serialize(),
+          success: function(data, textStatus) {
+            $("#result").text('create '+textStatus);
+            setTimeout(function() {
+              window.open("index.html", "_self");
+            }, 2000);
+          },
+          error: function(data, textStatus, errorThrown) {
+            console.log(errorThrown);
+          }
+        });
+      });
       break;
     case "modify":
+    case "delete":
+      console.log("modify");
       $.getJSON(
         "https://jsonplaceholder.typicode.com/users/" +
           localStorage.getItem("id"),
         loadForm
       );
+      if (localStorage.selector === "modify") {
+        $("#submit").click(function() {
+          $.ajax({
+            url:
+              "https://jsonplaceholder.typicode.com/users/" +
+              localStorage.getItem("id"),
+            type: "put",
+            data: $("#contact").serialize(),
+            success: function(data, textStatus) {
+              $("#result").text('modify '+ textStatus);
+              setTimeout(function() {
+                window.open("index.html", "_self");
+              }, 2000);
+            },
+            error: function(data, textStatus, errorThrown) {
+              console.log(errorThrown);
+            }
+          });
+        });
+      } else {
+        $("#submit").click(function() {
+          $.ajax({
+            url:
+              "https://jsonplaceholder.typicode.com/users/" +
+              localStorage.getItem("id"),
+            type: "delete",
+            data: $("#contact").serialize(),
+            success: function(data, textStatus) {
+              $("#result").text('delete '+textStatus);
+              setTimeout(function() {
+                window.open("index.html", "_self");
+              }, 2000);
+            },
+            error: function(data, textStatus, errorThrown) {
+              console.log(errorThrown);
+            }
+          });
+        });
+      }
       break;
-    case "delete":
-      console.log("delete");
-      break;
+    default:
   }
 });
