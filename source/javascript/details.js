2e4d070bec355a5302112ad5d80ce0305c470ac0
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2018-03-09 12:42:25 +0100

    ottimizzato js

--- source/javascript/details.js
+++ source/javascript/details.js
@@ -1,38 +1,18 @@
 /*global $ */
-function load(arrayId, arrayValue) {
-    "use strict";
-    $("#" + arrayId).text(arrayValue);
+function loadDetails(user) {
+  "use strict";
+  var arrayId = ["name", "username", "email", "address", "phone", "website"];
+  var arrayValue = [user.name,user.username,user.email,user.address.street + " " + user.address.city + " " + user.address.zipcode,user.phone,user.website];
+  for (var i = 0; i < arrayId.length; i++) {
+    /*jshint -W117 */ load(arrayId[i], arrayValue[i]);
   }
-function loadDetails(user){
-    "use strict";
-    var arrayId = [
-      "name",
-      "username",
-      "email",
-      "address",
-      "phone",
-      "website"
-    ];
-    var arrayValue = [
-      user.name,
-      user.username,
-      user.email,
-      user.address.street + ' ' + 
-      user.address.city + ' ' + 
-      user.address.zipcode,
-      user.phone,
-      user.website
-    ];
-    for (var i = 0; i < arrayId.length; i++) {
-      load(arrayId[i], arrayValue[i]);
-    }
-    $('.loader').fadeOut();
+  $(".loader").fadeOut();
 }
 window.addEventListener("load", function() {
-    //avvio funzione dopo il caricamento del DOM
-    "use strict";
-    $.getJSON("https://jsonplaceholder.typicode.com/users/"+localStorage.getItem('id'), loadDetails);
-    $('#back').click(function(){
-        window.open("index.html", "_self");
-    });
+  //avvio funzione dopo il caricamento del DOM
+  "use strict";
+  $.getJSON("https://jsonplaceholder.typicode.com/users/" + localStorage.getItem("id"),loadDetails);
+  $("#back").click(function() {
+    window.open("index.html", "_self");
+  });
 });
